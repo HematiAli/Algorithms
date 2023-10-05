c46bfd3290186f013cb6d2741d84c41c780f6098
Ali <alihemati@gmail.com>
2023-10-05 10:09:10 +0330

    Tow sum

---
+++ tow sum.py
@@ -0,0 +1,20 @@
+"""
+    Tow Sum
+            [2, 7, 11, 15], 9 =>[0, 1]
+"""
+
+def tow_sum(numbers, target):
+    p1 = 0
+    p2 = len(numbers) - 1
+    while p1 < p2:
+        s = numbers[p1] + numbers[p2]
+        if s == target:
+            return[p1, p2]
+        
+        elif s > target:
+            p2 -= 1
+        else:
+            p1 += 1
+
+
+print(tow_sum( [2, 7, 11, 15], 9))
